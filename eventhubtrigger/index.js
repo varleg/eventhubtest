fe28477dca83b0b894e08b6b014b262a28435b6f
johninge@varleg.no <john@varleg.no>
2020-10-07 10:23:11 +0200

    debug

--- eventhubtrigger/index.js
+++ eventhubtrigger/index.js
@@ -2,16 +2,16 @@ const { EventhubProducerClient } = require("@azure/event-hubs");
 const appInsights = require("applicationinsights");
 const connectionString = process.env.EVENTHUB_CONNECTION_STRING;
 const eventHubName = "functionappseventhub";
-module.exports = async function (context, req) {
+module.exports = async function (context) {
     appInsights.start();
-    const producer = new EventhubProducerClient(connectionString, eventHubName);
+    //const producer = new EventhubProducerClient(connectionString, eventHubName);
 
-    const batch = await producer.createBatch();
-    batch.tryAdd({ body: "first  http event" });
-    batch.tryAdd({ body: "second http  event" });
-    batch.tryAdd({ body: "third  http event" });
+    //const batch = await producer.createBatch();
+    //batch.tryAdd({ body: "first  http event" });
+    //batch.tryAdd({ body: "second http  event" });
+    //batch.tryAdd({ body: "third  http event" });
 
-    await producer.sendBatch(batch);
-    await producer.close();
+    //await producer.sendBatch(batch);
+    //await producer.close();
     context.log("A batch of three events was sent from azure function after trigger event");
 }
