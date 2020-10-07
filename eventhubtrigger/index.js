38e337a436359426ebc7c03faa3b6940c05b03a1
johninge@varleg.no <john@varleg.no>
2020-10-07 10:00:53 +0200

    added funtion to send events on http trigger

---
+++ eventhubtrigger/index.js
@@ -0,0 +1,15 @@
+const { EventhubProducerClient } = require("@azure/event-hubs")
+const connectionString = process.env.EVENTHUB_CONNECTION_STRING
+const eventHubName = "eventhubtestingforpx"
+module.exports = async function (context, req) {
+    const producer = new EventhubProducerClient(connectionString, eventHubName);
+
+    const batch = await producer.createBatch();
+    batch.tryAdd({ body: "first  http event" });
+    batch.tryAdd({ body: "second http  event" });
+    batch.tryAdd({ body: "third  http event" });
+
+    await producer.sendBatch(batch);
+    await producer.close();
+    context.log("A batch of three events was sent from azure function after trigger event");
+}
