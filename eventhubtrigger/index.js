1fba1e169f944799c3f340fc34cf74131b56e60b
johninge@varleg.no <john@varleg.no>
2020-10-07 13:07:14 +0200

    send with trigger

--- eventhubtrigger/index.js
+++ eventhubtrigger/index.js
@@ -1,10 +1,19 @@
+const { EventHubProducerClient } = require("@azure/event-hubs");
+const connectionString = process.env.EVENT_HUB_CONNECTION_STRING;
+const eventHubName = "functionappseventhub";
 module.exports = async function (context, req) {
+    const producer = new EventHubProducerClient(connectionString, eventHubName);
     context.log('JavaScript HTTP trigger function processed a request.');
+    const batch = await producer.createBatch();
+    batch.tryAdd({ body: "First event" });
+    batch.tryAdd({ body: "Second event" });
+    batch.tryAdd({ body: "Third event" });
 
-    const name = (req.query.name || (req.body && req.body.name));
-    const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-        : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    // Send the batch to the event hub.
+    await producer.sendBatch(batch);
+
+    // Close the producer client.
+    await producer.close();
 
     context.res = {
         // status: 200, /* Defaults to 200 */
